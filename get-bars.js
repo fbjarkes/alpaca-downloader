ecd25c3d8a91bc90d628bb0ea086c78f65407482
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2022-05-04 18:22:38 +0200

    Add optional '--days' parameter

--- get-bars.js
+++ get-bars.js
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
 const R = require('ramda');
-const Alpaca = require("@alpacahq/alpaca-trade-api");
+const Alpaca = require('@alpacahq/alpaca-trade-api');
 const dotenv = require('dotenv');
 const argv = require('yargs').argv;
 const fs = require('fs').promises;
 const logger = require('./logger');
 const { getSymbols, sleep } = require('./utils');
 
-const VALID_TIMEFRAMES = ['1min', '15min', '60min', 'day'];
+const VALID_TIMEFRAMES = [ '1min', '15min', '60min', 'day' ];
 const TIMEFRAME_MAPPING = {
     '1min': '1Min',
     '15min': '15Min',
     '60min': '1Hour',
-    'day': '1Day'
-}
+    day: '1Day'
+};
 const CONCURRENT_REQUESTS = 100;
 
 // eslint-disable-next-line no-undef
-const {parsed: cfg} = dotenv.config({path: `${__dirname}/.env`});    
+const { parsed: cfg } = dotenv.config({ path: `${__dirname}/.env` });
 
 const printUsage = () => {
     console.log('Usage:');
-    console.log('./get-bars.js --start <2021-01-01> --timeframe <1min|15min|60min|day> --limit <1...10000> --symbols AAPL,TSLA --symbolsFile sp500.txt');
-}
+    console.log(
+        './get-bars.js --start <2021-01-01> --timeframe <1min|15min|60min|day> --days <1...1000> --symbols AAPL,TSLA --symbolsFile sp500.txt'
+    );
+};
 
 const getDefaultStart = (timeframe) => {
     const epoch = new Date().getTime();
@@ -38,14 +40,14 @@ const getDefaultStart = (timeframe) => {
     if (timeframe === 'day') {
         return new Date(epoch - 360 * 24 * 60 * 60 * 1000); // 1 year
     }
-}
+};
 
 // Create a RFC3339 date string
 const createRFC3339DateString = (date) => {
     // TODO: always return UTC (Zulu), so just remove 4 or 5 hours depending on DST/EST
     let str;
     if (date.length === 10) {
-        str = `${date}T00:00:00Z`; 
+        str = `${date}T00:00:00Z`;
     } else if (date.length === 16) {
         // TODO: substract hours
         str = `${date.substr(0, 10)}T${date.substr(11, 16)}:00Z`;
@@ -57,52 +59,49 @@ const createRFC3339DateString = (date) => {
 
 const transformBarData = (barData) => {
     return {
-        'DateTime': barData.Timestamp,
-        'Open': barData.OpenPrice,
-        'High': barData.HighPrice,
-        'Low': barData.LowPrice,
-        'Close': barData.ClosePrice,
-        'Volume': barData.Volume,        
-    }
-}
+        DateTime: barData.Timestamp,
+        Open: barData.OpenPrice,
+        High: barData.HighPrice,
+        Low: barData.LowPrice,
+        Close: barData.ClosePrice,
+        Volume: barData.Volume
+    };
+};
 
 // Returns JSON object with symbol and array with OHLC data: { symbol: "AAPL", bars: [ {"Open": .., "High": .., "Low": .., "Close": .., "Volume": ..}]}
-const getBars = async (symbol, alpaca, {start, end, timeframe, limit, bulkDownload}) => {
+const getBars = async (symbol, alpaca, { start, end, timeframe, limit, bulkDownload }) => {
     logger.debug(`getBarsV2: symbol=${symbol}, start=${start}, end=${end}`);
-    const bars = []
+    const bars = [];
     const data = {
         symbol,
         bars
-    }
+    };
     try {
-        let res = alpaca.getBarsV2(
-            symbol,
-            {
-                start,
-                end,
-                limit,
-                timeframe,
-                adjustment: 'all'
-            },
-        );                
-        for await (let b of res){
-            bars.push(transformBarData(b));                        
+        let res = alpaca.getBarsV2(symbol, {
+            start,
+            end,
+            limit,
+            timeframe,
+            adjustment: 'all'
+        });
+        for await (let b of res) {
+            bars.push(transformBarData(b));
         }
-        
-        // TODO: 
+
+        // TODO:
         // if bulkDownload:
         //      while last bar timestamp < end:
         //          set start to last bar timestamp + 1min (if 1min timeframe) and call getBars again
         // NOTE: need to convert between UTC to compare to end??
-    } catch (error) {        
+    } catch (error) {
         logger.error(`${symbol}: ${error}`); // 422 error likely due to erroneous start/end parameter
-    } 
+    }
     return data;
 };
 
 const saveToFile = async (barsForSymbol) => {
     // Write JSON object with symbol and array with OHLCV data:
-    // { "AAPL": [ 
+    // { "AAPL": [
     //  {"Open": .., "High": .., "Low": .., "Close": .., "Volume": ..},
     //  {"Open": .., "High": .., "Low": .., "Close": .., "Volume": ..},
     //  {"Open": .., "High": .., "Low": .., "Close": .., "Volume": ..}
@@ -115,46 +114,53 @@ const saveToFile = async (barsForSymbol) => {
         const str = JSON.stringify(data, null, 2);
         await fs.writeFile(`${barsForSymbol.symbol}.json`, str);
         logger.info(`Wrote file (${Math.round(str.length / 1024, 0)}kb) '${barsForSymbol.symbol}.json'`);
-    }    
-}
+    }
+};
 
 const downloadAndSaveChunk = async (symbols, alpaca, options) => {
     try {
-        const barsForSymbols = await Promise.all(symbols.map(symbol => getBars(symbol, alpaca, options)));        
-        await Promise.allSettled (barsForSymbols.map(saveToFile));
+        const barsForSymbols = await Promise.all(symbols.map((symbol) => getBars(symbol, alpaca, options)));
+        await Promise.allSettled(barsForSymbols.map(saveToFile));
         return barsForSymbols;
-    } catch (err) {        
+    } catch (err) {
         logger.error(err);
     }
-}
+};
 
 (async () => {
     if (!(argv.symbols || argv.symbolsFile)) {
-        console.log('Missing --symbols parameter or --symbolsFile parameter')
+        console.log('Missing --symbols parameter or --symbolsFile parameter');
         printUsage();
         return 0;
     }
-    if (!(VALID_TIMEFRAMES.includes(argv.timeframe))) {
-        console.log('Missing valid --timeframe parameter', 'Valid: ', VALID_TIMEFRAMES)
+    if (!VALID_TIMEFRAMES.includes(argv.timeframe)) {
+        console.log('Missing valid --timeframe parameter', 'Valid: ', VALID_TIMEFRAMES);
         printUsage();
         return 0;
     }
-    
+
     let options;
     if (argv.start && argv.end) {
         options = {
             timeframe: TIMEFRAME_MAPPING[argv.timeframe],
             limit: 10000,
             start: createRFC3339DateString(argv.start),
-            end: createRFC3339DateString(argv.end),
-        }
+            end: createRFC3339DateString(argv.end)
+        };
+    } else if (argv.days) {
+        options = {
+            timeframe: TIMEFRAME_MAPPING[argv.timeframe],
+            //limit: 100000,
+            start: new Date(new Date().getTime() - argv.days * 24 * 60 * 60 * 1000), // start 'days' ago
+            end: new Date().toISOString() // end now
+        };
     } else {
         // Options for non-bulk download
         options = {
             timeframe: TIMEFRAME_MAPPING[argv.timeframe],
             limit: 10000,
             start: getDefaultStart(argv.timeframe).toISOString(),
-            end: new Date().toISOString(),
+            end: new Date().toISOString()
         };
     }
 
@@ -163,11 +169,11 @@ const downloadAndSaveChunk = async (symbols, alpaca, options) => {
         secretKey: cfg['SECRET_KEY'],
         paper: true,
         usePolygon: false
-      });
+    });
 
     const symbols = await getSymbols(argv.symbols, argv.symbolsFile);
     const chunks = R.splitEvery(CONCURRENT_REQUESTS, symbols);
-    for (let chunk of chunks) {        
+    for (let chunk of chunks) {
         await downloadAndSaveChunk(chunk, alpaca, options);
         await sleep(500);
     }
